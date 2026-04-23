643e67f00ec16e310865c6d8405a9bb95d8fbb12
frxnkdev <your-email@example.com>
2026-04-23 01:34:28 +0100

    FULL RESET railway working version

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("VERTIX WORKING");
+  res.send("WORKING");
 });
 
 app.get("/ping", (req, res) => {
@@ -11,11 +11,6 @@ app.get("/ping", (req, res) => {
 
 const PORT = process.env.PORT;
 
-if (!PORT) {
-  console.log("NO PORT FOUND");
-  process.exit(1);
-}
-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("VERTIX RUNNING ON", PORT);
+  console.log("RUNNING ON " + PORT);
 });
